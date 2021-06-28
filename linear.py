b818b13494b17b1b346dc5d4c42afa9afc504fd4
shopon mia <shopon5677@gmail.com>
2021-06-28 17:56:36 +0600

    linear search file set up done

--- linear.py
+++ linear.py
@@ -1,1 +1,17 @@
-hello
+def linear_search(list, search_number):
+    found = False
+    for i in range(0, len(list)):
+        if list[i] == search_number:
+            found = True
+            break
+    
+    if found == True:
+        print("Number is found at ",i, "index")
+    else:
+        print("Number is not found")
+
+
+
+list =[10, 30, 20, 5, 40]
+search_number = 20
+linear_search(list,search_number)
